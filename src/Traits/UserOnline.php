b448e4bbb41c17a4a435316b0e695cb29d521ef1
Karim Mohamed <komicho1996@gmail.com>
2019-02-24 22:08:42 +0200

    Optimization: do not show the status to the same user

--- src/Traits/UserOnline.php
+++ src/Traits/UserOnline.php
@@ -3,14 +3,20 @@
 namespace Komicho\Laravel\Traits;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Auth;
 
 trait UserOnline
 {
     public function isOnline()
     {
+        if (Auth::id() == $this->id) {
+            return false;
+        }
+
         if (Cache::has('userIsOnline-'.$this->id)) {
             return true;
         }
+        
         return false;
     }
 }
